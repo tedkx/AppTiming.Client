41b0a0e75bd9a3c6acb2a3ff1bf99fb1a657366c
tedkx <thodwriskalaitzidis@hotmail.com>
2015-11-30 14:19:19 +0200

    Implemented base API URL parameter, added xml summary documentation to public methods

--- readme.md
+++ readme.md
@@ -12,4 +12,8 @@ OR
 ##Usage
 ----
 Just initialize an AppTimingClient object with the desired API key **apptiming** endpoint.
-API keys can be managed from **apptiming** server.
+Then either call Start(), run operation then call End()
+OR
+Call TimeOperation() passing the method to run
+
+API keys refer to apps -not users- and can be managed from **apptiming** server.
--- src/AppTiming.Client/Client.cs
+++ src/AppTiming.Client/Client.cs
@@ -5,15 +5,21 @@ using System.Threading.Tasks;
 
 namespace AppTiming.Client
 {
+    /// <summary>
+    /// The client class that performs all calls.
+    /// Either call Start(), do the operation and call End()
+    /// OR
+    /// call TimeOperation() passing the action to keep time for
+    /// </summary>
     public class AppTimingClient
     {
         public const string DefaultApiEndpoint = "http://192.168.0.21:3000/api/v1/";
         public const string UrlRegex = @"(https?:\/\/)?([\da-z\.-]+)\.([0-9a-z\.]{1,6})(:[0-9]*)([\/\w \.-]*)*\/?";
 
         object _lock = new object();
-        private Uri _apiEndpoint;
         protected WebClient _client = null;
         public string ApiKey { get; private set; }
+        public readonly string ApiBaseUrl;
 
 
         public AppTimingClient(string apiKey)
@@ -28,38 +34,59 @@ namespace AppTiming.Client
             : this(apiKey, DefaultApiEndpoint, attachUnobservedExceptionHandler)
         { }
 
-        public AppTimingClient(string apiKey, string apiEndpoint, bool attachUnobservedExceptionHandler)
+        public AppTimingClient(string apiKey, string apiBaseUrl, bool attachUnobservedExceptionHandler)
         {
             ApiKey = apiKey;
-            if (string.IsNullOrWhiteSpace(apiEndpoint)) throw new ArgumentNullException("apiEndpoint", "API Endpoint cannot be empty");
-            if (!System.Text.RegularExpressions.Regex.IsMatch(apiEndpoint, UrlRegex)) throw new ArgumentException("apiEndpoint", "Invalid endpoint " + apiEndpoint);
+            if (string.IsNullOrWhiteSpace(apiBaseUrl)) throw new ArgumentNullException("apiEndpoint", "API Endpoint cannot be empty");
+            if (!apiBaseUrl.EndsWith("/")) apiBaseUrl += "/";
+            if (!apiBaseUrl.EndsWith("api/v1/")) apiBaseUrl += "api/v1/";
+            if (!System.Text.RegularExpressions.Regex.IsMatch(apiBaseUrl, UrlRegex)) throw new ArgumentException("apiEndpoint", "Invalid endpoint " + apiBaseUrl);
             _client = new WebClient() { Encoding = Encoding.UTF8 };
+            try { new Uri(apiBaseUrl); }
+            catch(Exception ex) { throw new ArgumentException("apiEndpoint", string.Format("Invalid endpoint {0}, {1}", apiBaseUrl, ex.Message)); }
+            ApiBaseUrl = apiBaseUrl;
             if (attachUnobservedExceptionHandler) TaskScheduler.UnobservedTaskException += GetUnobservedTaskExceptionHandler();
         }
 
+        /// <summary>
+        /// Start keeping time for an operation
+        /// </summary>
+        /// <param name="unitname">A desired identifier for the method keeping track of</param>
+        /// <returns></returns>
         public async Task<string> Start(string unitname)
         {
-            return await _createTask(unitname, "http://192.168.0.21:3000/api/v1/track/");
+            return await _createTask(unitname, ApiBaseUrl.ToString() + "track/" + unitname);
         }
 
+        /// <summary>
+        /// Stop keeping time for an operation
+        /// </summary>
+        /// <param name="unitname">The identifier used to Start() the time tracking</param>
+        /// <returns></returns>
         public async Task<string> End(string unitname)
         {
-            return await _createTask(unitname, "http://192.168.0.21:3000/api/v1/time/");
+            return await _createTask(unitname, ApiBaseUrl.ToString() + "time/" + unitname);
         }
 
         private Task<string> _createTask(string unitname, string url)
         {
             if (string.IsNullOrWhiteSpace(unitname)) throw new ArgumentNullException("unitname", "Unit name cannot be null");
             if (string.IsNullOrWhiteSpace(url)) throw new ArgumentNullException("url", "Url cannot be null");
-            if (!url.EndsWith("/")) url += "/";
+            Uri uri = new Uri(url);
             lock (_lock)
             {
                 _client.Headers.Remove("Authorization");
                 _client.Headers.Add("Authorization", Convert.ToBase64String(Encoding.UTF8.GetBytes("ApiKey " + ApiKey)));
-                return _client.UploadStringTaskAsync(new Uri("http://192.168.0.21:3000/api/v1/track/" + unitname), string.Empty);
+                return _client.UploadStringTaskAsync(uri, string.Empty);
             }
         }
 
+        /// <summary>
+        /// Keep time for an operation
+        /// </summary>
+        /// <param name="unitname">The identifier to use for the operation</param>
+        /// <param name="fn">The operation to run and keep time for</param>
+        /// <returns></returns>
         public async Task<string> TimeOperation(string unitname, Action fn)
         {
             await Start(unitname);
